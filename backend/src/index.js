095608ab11467e3ca08b0ddac28cd2c6bce1c9b9
Shashank <shashanksingh14370@gmail.com>
2025-06-26 17:43:41 +0530

    message routes setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 dotenv.config();
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js'
 import { connectDB } from './lib/db.js';
 
 const app=express();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use('/api/auth',authRoutes);
+app.use('/api/message',messageRoutes);
 const PORT=process.env.PORT;
 
 app.listen(5001,()=>{
