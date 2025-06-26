b4fbdc3f7bbfb6e7df1032d0f0fbc3193859a9b3
Shashank <shashanksingh14370@gmail.com>
2025-06-26 15:48:58 +0530

    backend and user auth setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,90 @@
+import express from 'express';
+import bcryptjs from 'bcryptjs';
+import User from '../models/user.model.js'
+import { generateToken } from '../lib/utils.js';
+export const signup=async(req,res)=>{
+    const {fullName,email,password}=req.body;
+    try {        
+        if(!email || !fullName || !password){
+            return res.status(400).json({
+                message:"All the fields are required"
+            })
+        }
+        if(password.length<6){
+            return res.status(400).json({
+                message:"password must be atleast 6 character long"
+            })
+        }
+        const user=await User.findOne({email});
+        if(user){
+            return res.status(400).json({message:"email already exists"})
+        }
+        const salt=await bcryptjs.genSalt(10);
+        const hashedPassword=await bcryptjs.hash(password,salt);
+        const newUser=new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+        if(newUser){
+            //generate jwt token
+            generateToken(newUser._id,res);
+            await newUser.save();
+            res.status(201).json({
+                _id:newUser._id,
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic
+            })
+        }
+        else{
+            res.status(400).json({message:"Invalid user data"})
+        }
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+export const login=async(req,res)=>{
+    const {email,password}=req.body;
+    try {        
+        if(!email || !password){
+            return req.status(400).json({
+                message:"Invalid Credentials"
+            })
+        }
+        const user=await User.findOne({email});
+        if(!user){
+            return req.status(400).json({
+                message:"Invalid Credentials"
+            })
+        }
+        
+        const checkPassword=bcryptjs.compare(password,user.password);
+        if(!checkPassword){
+            return req.status(400).json({
+                message:"Invalid Credentials"
+            })
+        }
+        generateToken(user._id,res);
+        req.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            profilePic: user.profilePic
+        })
+        
+    } catch (error) {
+        console.log("Error in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
+export const logout=async(req,res)=>{
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+        res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
